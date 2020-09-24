e365d221a61e205d92c26fa69ce71d42b31a4b59
sooyoungHo <sooyoung.jinong@gmail.com>
2020-09-24 11:12:46 +0900

    아무동작없음

---
+++ README.md
@@ -0,0 +1,28 @@
+제작자 : hodu
+이 프로젝트는 UI개발업무능력 향상을 위한 프로젝트입니다.
+기존에 했던 Vue.CLI todolist프로젝트를 응용할 예정입니다.
+
+# webpack-simple
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="Buttoncontainer">
+		<span class="clearAllBtn" @click="login">login</span><br>
+		<span class="clearAllBtn" @click="clearTodo">Create account</span>
+	</div>
+</template>
+<script>
+	export default{
+		methods:{
+			clearTodo(){
+				console.log("create Account");
+			//	this.$emit('removeAll');
+			},
+			login(){
+				console.log("login");
+			//	this.$emit('removeAll');
+			}
+		}
+	}
+</script>
+<style scoped>
+	.Buttoncontainer{
+		width:8.5rem;
+		height:50px;
+		line-height:50px;
+		background-color:white;
+		border-radius:5px;
+		margin:0 auto;
+	}
+	.clearAllBtn{
+		color: #e20303;
+		display : black;
+	}
+</style>
